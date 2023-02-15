817570e1add9fcea1df08d5106afec4120ae9ad4
ARRINT\ibarham <ibarham@arrowinternational.com>
2023-02-15 15:25:21 -0500

    Updating to start adding car stuff

--- Norseman/ViewModels/LandingPageViewModel.cs
+++ Norseman/ViewModels/LandingPageViewModel.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Norseman.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AndroidX.Navigation.NavType;
 
 namespace Norseman.ViewModels
 {
-    public class LandingPageViewModel
+    public class LandingPageViewModel : ViewModelBase
     {
+        public VehicleMake SelectedMake
+        {
+            get => _selectedMake;
+            set => SetProperty(ref _selectedMake, value);
+        }
+
+        public IEnumerable<VehicleMake> Makes
+        {
+            get
+            {
+                return Enum.GetValues(typeof(VehicleMake))
+                    .Cast<VehicleMake>();
+            }
+        }
+        public LandingPageViewModel() { }
+
+        private VehicleMake _selectedMake;
     }
 }
