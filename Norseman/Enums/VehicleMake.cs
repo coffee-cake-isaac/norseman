9581855d001df4a9f14135c04f2e099c6af92223
ARRINT\ibarham <ibarham@arrowinternational.com>
2023-02-15 16:28:05 -0500

    Adding missing items

---
+++ Norseman/Enums/VehicleMake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Norseman.Enums
+{
+    public enum VehicleMake
+    {
+        AUDI,
+        BMW,
+        CADILLAC,
+        CHEVROLET,
+        FORD,
+        GENESIS,
+        GMC,
+        HONDA,
+        HYUNDAI,
+        JAGUAR,
+        KIA,
+        LUCID,
+        MERCEDES,
+        MINI,
+        NISSAN,
+        POLESTAR,
+        PORSCHE,
+        RAM,
+        RIVIAN,
+        SUBARU,
+        TESLA,
+        TOYOTA,
+        VOLKSWAGEN,
+        VOLVO,
+    }
+}
---
+++ Norseman/Models/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using Norseman.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Norseman.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public VehicleMake Make { get; set; }
+
+        public string Model { get; set; }
+    }
+}
