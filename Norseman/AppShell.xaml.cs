8c95c3342fb99e196319056807e5ecc5180fd81d
coffee-cake-isaac <48419673+coffee-cake-isaac@users.noreply.github.com>
2023-02-14 22:51:23 -0500

    Updating for preliminary routing for new pages... seems super simple

--- Norseman/AppShell.xaml.cs
+++ Norseman/AppShell.xaml.cs
@@ -5,6 +5,7 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
+		Routing.RegisterRoute("LandingPageView", typeof(Views.LandingPageView));
 	}
 }
 
---
+++ Norseman/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Norseman.ViewModels
+{
+	public class MainPageViewModel : ViewModelBase
+	{
+		public ICommand NavigateToLandingPageView => new Command(NavigateToLandingPageViewCommand);
+
+        private async void NavigateToLandingPageViewCommand(object obj)
+        {
+			await Shell.Current.GoToAsync("LandingPageView");
+		}
+
+        public MainPageViewModel()
+		{
+		}
+	}
+}
+
